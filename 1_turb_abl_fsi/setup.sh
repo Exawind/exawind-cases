5323ea9b4128935e624e028b8fcf3a5052f7d018
psakievich <psakiev@sandia.gov>
2023-06-26 08:10:50 -0400

    Less verbose output of spack stuff

--- 1_turb_abl_fsi/setup.sh
+++ 1_turb_abl_fsi/setup.sh
@@ -1,7 +1,7 @@
 # run this from inside your fsi spack environment
-set -x
 spack load openfast
 # build controller shared lib
+set -x
 cd 5MW_Baseline/ServoData/
 spack build-env openfast fc -shared  -o libdiscon.so DISCON/DISCON.F90 
 # run standalone openfast and copy files to run directory
