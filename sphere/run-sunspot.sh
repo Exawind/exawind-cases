ced60d40f6ac2f1b011c56014278cde3bb8a55f7
Jon Rood <jon.rood@nrel.gov>
2023-07-10 14:40:22 -0600

    Update sunspot script for sphere.

--- sphere/run-sunspot.sh
+++ sphere/run-sunspot.sh
@@ -4,7 +4,6 @@
 #PBS -q workq
 #PBS -l select=1
 #PBS -l walltime=30:00
-#PBS -l filesystems=home
 #PBS -j oe
 
 set -e
@@ -15,10 +14,9 @@ cmd() {
 }
 
 cmd "cd $PBS_O_WORKDIR"
-cmd "export SPACK_MANAGER=${HOME}/exawind/spack-manager"
+cmd "export SPACK_MANAGER=/lus/gila/projects/CSC249ADSE13_CNDA/${USER}/exawind/spack-manager"
 cmd "source ${SPACK_MANAGER}/start.sh && spack-start"
 cmd "spack env activate -d ${SPACK_MANAGER}/environments/exawind-sunspot"
 cmd "spack load exawind+amr_wind_gpu~nalu_wind_gpu"
-cmd "spack load trilinos"
 cmd "which exawind"
 cmd "mpiexec -n 2 -ppn 2 exawind --awind 1 --nwind 1 sphere.yaml"
