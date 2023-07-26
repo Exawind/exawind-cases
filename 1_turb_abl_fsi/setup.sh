363dbd2df95029d6c44f5681304d935ef6c18290
psakievich <psakiev@sandia.gov>
2023-07-26 15:28:42 -0600

    Make FSI case more flexible (#4)
    * Start template based execution
    
    * Scripting process for automated submissions
    
    * Auto populate looks good for frontier
    
    * Minor tweak
    
    * Update turbine position
    
    * Add eagle config
    
    * Update README
    
    * Add ignore file
    
    * Tested on frontier
    
    * delete unused files and account for static box

--- 1_turb_abl_fsi/setup.sh
+++ 1_turb_abl_fsi/setup.sh
@@ -1,12 +1,65 @@
-# run this from inside your fsi spack environment
-spack load openfast
-# build controller shared lib
-set -x
-rundir=$(pwd)
-cd ../5MW_Baseline/ServoData/
-spack build-env openfast fc -shared -fPIC -o libdiscon.so DISCON/DISCON.F90 
+# step 1 checks
+source ${SPACK_MANAGER}/start.sh
+spack-start # ensure spack-manager is going
+
+# machine specific params i.e. mesh/restart/etc
+aprepro_include=template_files/${SPACK_MANAGER_MACHINE}_aprepro.txt
+
+# step 2 parse
+for i in "$@"; do
+    case "$1" in
+        -n=*|--name=*)
+            PROBNAME="${i#*=}"
+            shift # past argument=value
+            ;;
+        -e=*|--email=*)
+            EMAIL="${i#*=}"
+            shift # past argument=value
+            ;;
+        -t=*|--tower=*)
+            TOWER="${i#*=}"
+            shift # past argument=value
+            ;;
+        -s=*|--submit=*)
+            SUBMIT="${i#*=}"
+            shift # past argument=value
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+
+# step 3 create dirs
+if [[ -z ${PROBNAME} ]]; then
+  PROBNAME=$(date "+%Y%m%d")
+fi
+rundir="$(pwd)/run_${PROBNAME}"
+mkdir -p ${rundir}
+
+# step 4 setup run dir
+echo "Setting up run dir"
+spack build-env trilinos aprepro -qW --include ${aprepro_include} TOWER=${TOWER} template_files/nrel5mw_nalu.yaml ${rundir}/nrel5mw_nalu.yaml
+spack build-env trilinos aprepro -qW --include ${aprepro_include} template_files/nrel5mw_amr.inp ${rundir}/nrel5mw_amr.inp
+spack build-env trilinos aprepro -qW --include ${aprepro_include} EMAIL=${EMAIL} NAME="fsi-${PROBNAME}" template_files/slurm_sub.sh ${rundir}/slurm_sub.sh
+cp template_files/nrel5mw.yaml ${rundir}
+cp template_files/hypre_file.yaml ${rundir}
+cp template_files/static_box.txt ${rundir}
+cp openfast_run/* ${rundir}
+
+# step 5 run openfast
+echo "Compile openfast servo"
+cd $(pwd)/../5MW_Baseline/ServoData/
+spack build-env openfast fc -shared -fPIC -o libdiscon.so DISCON/DISCON.F90
 # run standalone openfast and copy files to run directory
-cd $rundir/openfast_run/
-openfastcpp inp.yaml
-cp *.nc ../turb_run/
-cp *.chkp* ../turb_run/
+echo "Run openfast to start"
+set -x
+cd ${rundir}
+spack build-env openfast openfastcpp inp.yaml
+
+# step 6 submit job if desired
+if [ -n "${SUBMIT}" ]; then
+  echo "Submit job"
+  sbatch slurm_sub.sh
+fi
---
+++ 1_turb_abl_fsi/template_files/slurm_sub.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+{SLURM_ARGS}
+#SBATCH -o {NAME}.o%j
+#SBATCH -J {NAME}
+{if(EMAIL)}
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user={EMAIL}
+{endif}
+
+nodes=$SLURM_JOB_NUM_NODES
+ranks=$SLURM_NTASKS
+
+# env variables for the spack environment will propogate to a new shell
+# commands (i.e. spack) most likely do not
+source $SPACK_MANAGER/start.sh
+spack load exawind
+
+{if(SUBMIT_EXPORTS)}
+{SUBMIT_EXPORTS}
+{endif}
+
+{SRUN_COMMAND}
+
+mkdir run_$SLURM_JOBID
+#
+mv nrel5mw_nalu*.log run_$SLURM_JOBID
+mv 1t-abl_gpu_2048_128.o$SLURM_JOBID run_$SLURM_JOBID
+mv amr-wind.log run_$SLURM_JOBID/amr_wind_2048.log
+mv timings.dat run_$SLURM_JOBID/
+mv forces*dat run_$SLURM_JOBID/
+
