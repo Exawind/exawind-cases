9e19a17c6f432c6495f247afa332d2a53c491ed9
psakievich <psakiev@sandia.gov>
2023-07-26 14:37:36 -0400

    Scripting process for automated submissions

--- 1_turb_abl_fsi/setup.sh
+++ 1_turb_abl_fsi/setup.sh
@@ -1,12 +1,61 @@
-# run this from inside your fsi spack environment
-spack load openfast
-# build controller shared lib
-set -x
-rundir=$(pwd)
+# step 1 checks
+source ${SPACK_MANAGER}/start.sh
+spack-start # ensure spack-manager is going
+
+# machine specific params i.e. mesh/restart/etc
+aprepro_include=template_files/${SPACK_MANAGER_MACHINE}_aprepro.txt
+
+# step 2 parse
+for i in "$@"; do
+    case "$1" in
+        -n=*|--name=*)
+            PROBNAME="${i#*=}"
+            shift # past argument=value
+            ;;
+        -e=*|--email=*)
+            EMAIL="${i#*=}"
+            shift # past argument=value
+            ;;
+        -t=*|--tower=*)
+            TOWER="${i#*=}"
+            shift # past argument=value
+            ;;
+        -s=*|--submit=*)
+            SUBMIT="${i#*=}"
+            shift # past argument=value
+            ;;
+        --)
+            shift
+            break
+            ;;
+    esac
+done
+# step 3 create dirs
+if [[ -z ${PROBNAME} ]]; then
+  PROBNAME=$(date "+%Y%m%d")
+fi
+rundir="run_${PROBNAME}"
+mkdir -p ${rundir}
+
+# step 4 setup build dir
+echo "Setting up build dir"
+spack build-env trilinos aprepro -qW --include ${aprepro_include} TOWER=${TOWER} template_files/nrel5mw_nalu.yaml ${rundir}/nrel5mw_nalu.yaml
+spack build-env trilinos aprepro -qW --include ${aprepro_include} template_files/nrel5mw_amr.inp ${rundir}/nrel5mw_amr.inp
+spack build-env trilinos aprepro -qW --include ${aprepro_include} NAME=${PROBNAME}  template_files/slurm_sub.sh ${rundir}/slurm_sub.sh
+cp template_files/nrel5mw.yaml ${rundir}
+cp template_files/hypre_file.yaml ${rundir}
+cp openfast_run/* ${rundir}
+exit
+
+# step 5 run openfast
 cd ../5MW_Baseline/ServoData/
 spack build-env openfast fc -shared -fPIC -o libdiscon.so DISCON/DISCON.F90 
 # run standalone openfast and copy files to run directory
-cd $rundir/openfast_run/
-openfastcpp inp.yaml
-cp *.nc ../turb_run/
-cp *.chkp* ../turb_run/
+set -x
+cd $rundir/
+spack build-env openfast openfastcpp inp.yaml
+
+# step 6 submit job if desired
+if [ -n "${SUBMIT}" ]; then
+  sbatch slurm_sub.sh
+fi
