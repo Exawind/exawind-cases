29d0e0ebe0f8c5740cc327b5228898913081a599
psakiev <psakiev@sandia.gov>
2023-09-26 15:49:29 -0600

    Add scripting for the load blending time

--- 1_turb_abl_fsi/setup.sh
+++ 1_turb_abl_fsi/setup.sh
@@ -1,12 +1,14 @@
 # step 1 checks
-SPACK_MANAGER=/lustre/orion/proj-shared/cfd116/mullowne/spack-manager/
 source ${SPACK_MANAGER}/start.sh
 spack-start # ensure spack-manager is going
-spack env activate -d $SPACK_MANAGER/environments/exawind_rocm-5.4.3
 spack load exawind
 
 # machine specific params i.e. mesh/restart/etc
 aprepro_include=template_files/${SPACK_MANAGER_MACHINE}_aprepro.txt
+
+# Defaults for load blending (units are revolutions)
+MEAN_CUT_IN=3.0 # middle of hyperbolic tangent
+DELTA_CUT_IN=0.25 # width of hyperbolid tangent
 
 # step 2 parse
 for i in "$@"; do
@@ -21,6 +23,14 @@ for i in "$@"; do
             ;;
         -t=*|--tower=*)
             TOWER="${i#*=}"
+            shift # past argument=value
+            ;;
+        -d=*|--delta_cut_in_rev=*)
+            DELTA_CUT_IN="${i#*=}"
+            shift # past argument=value
+            ;;
+        -m=*|--mean_cut_in_rev=*)
+            MEAN_CUT_IN="${i#*=}"
             shift # past argument=value
             ;;
         -s=*|--submit=*)
@@ -45,8 +55,9 @@ mkdir -p ${rundir}
 echo "Setting up run dir"
 cp template_files/static_files/* ${rundir}
 cp template_files/${SPACK_MANAGER_MACHINE}_static_box.txt ${rundir}/static_box.txt
-spack build-env trilinos aprepro -qW --include ${aprepro_include} TOWER=${TOWER} template_files/nrel5mw_nalu.yaml ${rundir}/nrel5mw_nalu.yaml
+spack build-env trilinos aprepro -qW --include ${aprepro_include} CUT_IN_MEAN=${MEAN_CUT_IN} CUT_IN_WIDTH=${DELTA_CUT_IN} TOWER=${TOWER} template_files/nrel5mw_nalu.yaml ${rundir}/nrel5mw_nalu.yaml
 spack build-env trilinos aprepro -qW --include ${aprepro_include} TOWER=${TOWER} template_files/NRELOffshrBsline5MW_Onshore_ElastoDyn_BDoutputs.dat ${rundir}/NRELOffshrBsline5MW_Onshore_ElastoDyn_BDoutputs.dat
+spack build-env trilinos aprepro -qW  CUT_IN_MEAN=${MEAN_CUT_IN} CUT_IN_WIDTH=${DELTA_CUT_IN} template_files/inp.yaml ${rundir}/inp.yaml
 # getting a weird carriage on frontier in this file for some reason
 sed -i 's/\r//g' ${rundir}/NRELOffshrBsline5MW_Onshore_ElastoDyn_BDoutputs.dat
 spack build-env trilinos aprepro -qW --include ${aprepro_include} template_files/nrel5mw_amr.inp ${rundir}/nrel5mw_amr.inp
--- 4_turb_abl_fsi/setup.sh
+++ 4_turb_abl_fsi/setup.sh
@@ -6,6 +6,10 @@ spack load exawind
 # machine specific params i.e. mesh/restart/etc
 aprepro_include=template_files/${SPACK_MANAGER_MACHINE}_aprepro.txt
 
+# Defaults for load blending (units are revolutions)
+MEAN_CUT_IN=3.0 # middle of hyperbolic tangent
+DELTA_CUT_IN=0.25 # width of hyperbolid tangent
+
 # step 2 parse
 for i in "$@"; do
     case "$1" in
@@ -19,6 +23,14 @@ for i in "$@"; do
             ;;
         -s=*|--submit=*)
             SUBMIT="${i#*=}"
+            shift # past argument=value
+            ;;
+        -d=*|--delta_cut_in_rev=*)
+            DELTA_CUT_IN="${i#*=}"
+            shift # past argument=value
+            ;;
+        -m=*|--mean_cut_in_rev=*)
+            MEAN_CUT_IN="${i#*=}"
             shift # past argument=value
             ;;
         --)
@@ -48,7 +60,8 @@ cp template_files/static_files/* ${rundir}
 cp template_files/${SPACK_MANAGER_MACHINE}_static_box.txt ${rundir}/static_box.txt
 
 # files that need to be edited with the preprocessor, mainly for reuse from the single turbine case
-spack build-env trilinos aprepro -qW --include ${aprepro_include} template_files/nrel5mw_nalu.yaml ${rundir}/nrel5mw_nalu.yaml
+spack build-env trilinos aprepro -qW --include ${aprepro_include} CUT_IN_MEAN=${MEAN_CUT_IN} CUT_IN_WIDTH=${DELTA_CUT_IN} template_files/nrel5mw_nalu.yaml ${rundir}/nrel5mw_nalu.yaml
+spack build-env trilinos aprepro -qW  CUT_IN_MEAN=${MEAN_CUT_IN} CUT_IN_WIDTH=${DELTA_CUT_IN} template_files/inp.yaml ${rundir}/inp.yaml
 spack build-env trilinos aprepro -qW --include ${aprepro_include} ${single_turb_ref}/nrel5mw_amr.inp ${rundir}/nrel5mw_amr.inp
 spack build-env trilinos aprepro -qW --include ${aprepro_include} EMAIL=${EMAIL} NAME="fsi-${PROBNAME}" template_files/slurm_sub.sh ${rundir}/slurm_sub.sh
 
