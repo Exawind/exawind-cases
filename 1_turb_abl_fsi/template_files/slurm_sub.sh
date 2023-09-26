52f20eeb525524e1f752ad8ab766c62a7134e129
psakiev <psakiev@sandia.gov>
2023-09-26 16:16:37 -0600

    Revert accidental changes

--- 1_turb_abl_fsi/template_files/slurm_sub.sh
+++ 1_turb_abl_fsi/template_files/slurm_sub.sh
@@ -12,10 +12,7 @@ ranks=$SLURM_NTASKS
 
 # env variables for the spack environment will propogate to a new shell
 # commands (i.e. spack) most likely do not
-SPACK_MANAGER=/lustre/orion/proj-shared/cfd116/mullowne/spack-manager/
-source ${SPACK_MANAGER}/start.sh
-spack-start # ensure spack-manager is going
-spack env activate -d $SPACK_MANAGER/environments/exawind_rocm-5.4.3
+source $SPACK_MANAGER/start.sh
 spack load exawind
 
 {if(SUBMIT_EXPORTS)}
