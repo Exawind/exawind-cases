aaa07442fc3ca1c55d879171b07d78dd0c273588
Jon Rood <jon.rood@nrel.gov>
2024-04-11 15:19:35 -0600

    Updates to single-turbine for Sunspot (#24)

--- single-turbine/run-sunspot.sh
+++ single-turbine/run-sunspot.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/bash -l
 
 #PBS -A CSC249ADSE13_CNDA
 #PBS -q workq
-#PBS -l select=4
+#PBS -l select=2
 #PBS -l walltime=30:00
 #PBS -j oe
 
@@ -13,15 +13,39 @@ cmd() {
   eval "$@"
 }
 
-cmd "cd $PBS_O_WORKDIR"
-cmd "export SPACK_MANAGER=/lus/gila/projects/CSC249ADSE13_CNDA/${USER}/exawind/spack-manager"
-cmd "source ${SPACK_MANAGER}/start.sh && spack-start"
-cmd "spack env activate -d ${SPACK_MANAGER}/environments/exawind-sunspot"
+#PBS does not appear to have an easily accesible env variable for this
+NUM_NODES=2
+SPACK_ENV_NAME=exawind-sunspot
+MESH_PATH=/lus/gila/projects/CSC249ADSE13_CNDA/jrood/exawind/files
+MY_EXAWIND_MANAGER=/lus/gila/projects/CSC249ADSE13_CNDA/jrood/exawind/exawind-manager
+
+cmd "module unload oneapi"
+cmd "module load oneapi/release/2023.10.15.001"
+cmd "module unload intel_compute_runtime"
+cmd "module load intel_compute_runtime/release/agama-devel-647"
+
+cmd "cd ${PBS_O_WORKDIR}"
+cmd "export SPACK_PYTHON=python3.10"
+cmd "export EXAWIND_MANAGER=${MY_EXAWIND_MANAGER}"
+cmd "source ${EXAWIND_MANAGER}/start.sh && spack-start"
+cmd "spack env activate ${SPACK_ENV_NAME}"
 cmd "spack load exawind+amr_wind_gpu~nalu_wind_gpu"
-cmd "spack load trilinos"
 cmd "which exawind"
-cmd "rm -rf mesh"
-cmd "mkdir mesh"
-cmd "mpiexec -np 12 -ppn 12 stk_balance.exe -o ./mesh/ -i ../meshes/nrel5mw_nearbody.exo"
-cmd "mpiexec -np 48 -ppn 12 -envall gpu_tile_compact.sh exawind --awind 36 --nwind 12 nrel5mw.yaml"
-cmd "rm -r mesh"
+
+#Current possibly necessary exports
+#cmd "export FI_MR_CACHE_MONITOR=memhooks"
+#cmd "export FI_CXI_RX_MATCH_MODE=software"
+#cmd "export MPICH_SMP_SINGLE_COPY_MODE=NONE"
+#cmd "export MPIR_CVAR_ENABLE_GPU=0"
+
+#Update mesh path
+sed -i "s|CHANGE_PATH|${MESH_PATH}|g" nrel5mw_nalu.yaml || true
+
+#+amr_wind_gpu~nalu_wind_gpu
+cmd "python3.10 rank_file.py ${NUM_NODES}"
+cmd "cat exawind.rank_file | sort -g > tmp.txt && mv tmp.txt exawind.rank_file"
+AWIND_RANKS=$((${NUM_NODES}*12))
+NWIND_RANKS=$((${NUM_NODES}*44))
+TOTAL_RANKS=$((${NUM_NODES}*56))
+
+cmd "mpiexec -np ${TOTAL_RANKS} -ppn 56 --rankfile exawind.rank_file -envall gpu_tile_compact.sh exawind --awind ${AWIND_RANKS} --nwind ${NWIND_RANKS} nrel5mw.yaml"
