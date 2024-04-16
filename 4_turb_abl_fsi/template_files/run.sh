f296de5f28777100d136982bd3261d89bde4ebd9
Jon Rood <jon.rood@nrel.gov>
2024-04-16 13:14:41 -0600

    Escape braces.

--- 4_turb_abl_fsi/template_files/run.sh
+++ 4_turb_abl_fsi/template_files/run.sh
@@ -3,19 +3,19 @@
 
 set -e
 
-cmd() {
+cmd() \{
   echo "+ $@"
   eval "$@"
-}
+\}
 
 {if(SUBMIT_EXPORTS)}
 {SUBMIT_EXPORTS}
 {endif}
 
-cmd "export EXAWIND_MANAGER=${MY_EXAWIND_MANAGER}"
-cmd "source ${EXAWIND_MANAGER}/start.sh && spack-start"
-cmd "spack env activate ${SPACK_ENV_NAME}"
-cmd "spack load ${EXAWIND_EXE}"
+cmd "export EXAWIND_MANAGER=$\{MY_EXAWIND_MANAGER\}"
+cmd "source $\{EXAWIND_MANAGER\}/start.sh && spack-start"
+cmd "spack env activate $\{SPACK_ENV_NAME\}"
+cmd "spack load $\{EXAWIND_EXE\}"
 cmd "which exawind"
 
 {RUN_COMMAND_OPENFAST}
