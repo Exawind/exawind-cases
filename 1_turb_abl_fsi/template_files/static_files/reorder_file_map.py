08c75f75954392bc6e5d61323c53918df998964f
Jon Rood <jon.rood@nrel.gov>
2024-04-18 21:33:45 -0600

    Update reorder script.

--- 1_turb_abl_fsi/template_files/static_files/reorder_file_map.py
+++ 1_turb_abl_fsi/template_files/static_files/reorder_file_map.py
@@ -6,69 +6,69 @@ from io import StringIO
 
 table = StringIO("""
 hwt,idx,gpu,app
-000,000,4,blank
-008,001,4,nwind
-016,002,4,blank
-024,003,4,nwind
-032,004,4,blank
-040,005,4,nwind
-048,006,4,blank
-056,007,4,awind
-001,008,5,blank
+000,000,4,nwind
+001,008,4,nwind
+002,016,4,nwind
+003,024,4,nwind
+004,032,4,nwind
+005,040,4,nwind
+006,048,4,nwind
+007,056,4,awind
+008,001,5,nwind
 009,009,5,nwind
-017,010,5,blank
-025,011,5,nwind
-033,012,5,blank
-041,013,5,nwind
-049,014,5,blank
-057,015,5,awind
-002,016,2,blank
-010,017,2,nwind
-018,018,2,blank
-026,019,2,nwind
-034,020,2,blank
-042,021,2,nwind
-050,022,2,blank
-058,023,2,awind
-003,024,3,blank
-011,025,3,nwind
-019,026,3,blank
+010,017,5,nwind
+011,025,5,nwind
+012,033,5,nwind
+013,041,5,nwind
+014,049,5,nwind
+015,057,5,awind
+016,002,2,nwind
+017,010,2,nwind
+018,018,2,nwind
+019,026,2,nwind
+020,034,2,nwind
+021,042,2,nwind
+022,050,2,nwind
+023,058,2,awind
+024,003,3,nwind
+025,011,3,nwind
+026,019,3,nwind
 027,027,3,nwind
-035,028,3,blank
-043,029,3,nwind
-051,030,3,blank
-059,031,3,awind
-004,032,6,blank
-012,033,6,nwind
-020,034,6,blank
-028,035,6,nwind
-036,036,6,blank
-044,037,6,nwind
-052,038,6,blank
-060,039,6,awind
-005,040,7,blank
-013,041,7,nwind
-021,042,7,blank
-029,043,7,nwind
-037,044,7,blank
+028,035,3,nwind
+029,043,3,nwind
+030,051,3,nwind
+031,059,3,awind
+032,004,6,nwind
+033,012,6,nwind
+034,020,6,nwind
+035,028,6,nwind
+036,036,6,nwind
+037,044,6,nwind
+038,052,6,nwind
+039,060,6,awind
+040,005,7,nwind
+041,013,7,nwind
+042,021,7,nwind
+043,029,7,nwind
+044,037,7,nwind
 045,045,7,nwind
-053,046,7,blank
-061,047,7,awind
-006,048,0,blank
-014,049,0,nwind
-022,050,0,blank
-030,051,0,nwind
-038,052,0,blank
-046,053,0,nwind
-054,054,0,blank
-062,055,0,awind
-007,056,1,blank
-015,057,1,nwind
-023,058,1,blank
-031,059,1,nwind
-039,060,1,blank
-047,061,1,nwind
-055,062,1,blank
+046,053,7,nwind
+047,061,7,awind
+048,006,0,nwind
+049,014,0,nwind
+050,022,0,nwind
+051,030,0,nwind
+052,038,0,nwind
+053,046,0,nwind
+054,054,0,nwind
+055,062,0,awind
+056,007,1,nwind
+057,015,1,nwind
+058,023,1,nwind
+059,031,1,nwind
+060,039,1,nwind
+061,047,1,nwind
+062,055,1,nwind
 063,063,1,awind
 """)
 
@@ -93,11 +93,11 @@ print(f"AMR-Wind rank total: {amr_wind_ranks_total}")
 print(f"Nalu-Wind rank total: {nalu_wind_ranks_total}")
 print(f"Total ranks: {ranks_total}")
 
-mymap.loc[mymap['app'] == 'nwind', 'aidx'] = numpy.arange(nalu_wind_ranks_per_node)
-mymap.loc[mymap['app'] == 'nwind', 'aidx'] += amr_wind_ranks_total
+mymap.loc[mymap['app'] == 'nwind', 'aidx'] = numpy.arange(nalu_wind_ranks_per_node) + amr_wind_ranks_total
 mymap.loc[mymap['app'] == 'awind', 'aidx'] = numpy.arange(amr_wind_ranks_per_node)
 mymap.loc[mymap['app'] == 'blank', 'aidx'] = ''
 mymap.sort_values(by=['idx'],inplace=True)
+#print(mymap.to_string())
 
 with open('exawind.reorder_file', "w") as f:
     for node in range(nodes):
