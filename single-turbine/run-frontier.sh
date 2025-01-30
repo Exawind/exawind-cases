b18fec243ba75107e5d501ee38a48de8b41091ee
Gopal Yalla <gyalla@sandia.gov>
2025-01-30 13:42:29 -0500

    Updating frontier submission script to automatically load modules and spack packages

--- single-turbine/run-frontier.sh
+++ single-turbine/run-frontier.sh
@@ -6,7 +6,7 @@
 #SBATCH -t 30
 #SBATCH -q debug
 #SBATCH -S 0
-#SBATCH -N 2
+#SBATCH -N 4
 
 set -e
 
@@ -15,18 +15,48 @@ cmd() {
   eval "$@"
 }
 
-SPACK_ENV_NAME=exawind-frontier
+SPACK_ENV_NAME=exawind_01_29_2025
+EXAWIND_MANAGER=${MEMBERWORK}/cfd162/exawind-manager-builds/exawind-manager/
 MESH_PATH=${PROJWORK}/cfd162/shared
 
-cmd "module load PrgEnv-amd/8.5.0"
-cmd "module load amd/6.0.0"
-cmd "module load cray-mpich/8.1.27"
 cmd "module load cray-python"
-cmd "export EXAWIND_MANAGER=${PROJWORK}/cfd162/${USER}/exawind-manager"
 cmd "source ${EXAWIND_MANAGER}/start.sh && spack-start"
 cmd "spack env activate ${SPACK_ENV_NAME}"
-cmd "spack load exawind+amr_wind_gpu+nalu_wind_gpu"
-cmd "which exawind"
+cmd "module purge"
+
+#load_packages=("exawind", "amr-wind", "nalu-wind") #uncomment to only load specific root packages 
+root_specs=$(spack find | grep '[+]' | cut -d' ' -f2) #get root spec names from spack find
+# Loop through all packages
+for package in $root_specs; do
+    loaded=false
+    # If load_packages is undefined, load all root specs
+    if [ ${#load_packages[@]} -eq 0 ]; then
+        cmd "spack load "$package""
+        loaded=true
+
+    # Else, only load packages in load_packages variable 
+    else
+        for load_package in "${load_packages[@]}"; do
+            if [[ "$package" == *"$load_package"* ]]; then
+                cmd "spack load "$package""
+                loaded=true
+                break
+            fi
+        done
+    fi
+
+    # Load all modules for each package from the build environment
+    if [ "$loaded" = true ]; then
+        mods=$(spack build-env "$package" | grep 'LOADEDMODULES' | cut -d'=' -f2)
+        IFS=':' read -r -a modules <<< "$mods"
+        for module in "${modules[@]}"; do
+            # Check if the module is already loaded
+            if ! module list 2>&1 | grep -q "$module"; then
+                cmd "module load "$module""
+            fi
+        done
+    fi
+done
 
 #Current possibly necessary exports
 cmd "export FI_MR_CACHE_MONITOR=memhooks"
