348ea9ac847bc405841ad1689d52e662aa50ce5b
Gopal Yalla <gyalla@sandia.gov>
2025-08-04 08:19:24 -0600

    Run Frontier Scripts (#28)
    * Updating frontier submission script to automatically load modules and spack packages
    
    * Updating run scripts on frontier for single-turbine, sphere, and 4_turb_abl_rigid, cases
    
    * Updating run frontier scripts to use amr-wind and nalu on GPUs
    
    * Using spack build-env to run exawind and set up environment instead of loading modules directly
    
    * Exporting EXAWIND_MANAGER variable in run scripts

--- sphere/run-frontier.sh
+++ sphere/run-frontier.sh
@@ -2,10 +2,11 @@
 
 #SBATCH -o %x.o%j
 #SBATCH -A CFD162
-#SBATCH -t 00:30:00
+#SBATCH -t 01:00:00
 #SBATCH -q debug
 #SBATCH -S 0
-#SBATCH -N 1
+#SBATCH -J sphere
+#SBATCH -N 4
 
 set -e
 
@@ -14,23 +15,37 @@ cmd() {
   eval "$@"
 }
 
-SPACK_ENV_NAME=exawind-env
-MY_EXAWIND_MANAGER=${PROJWORK}/cfd162/${USER}/exawind-manager
-EXAWIND_EXE=exawind~amr_wind_gpu~nalu_wind_gpu
-cmd "module load PrgEnv-gnu-amd/8.6.0"
-cmd "module load amd-mixed/6.1.3"
-cmd "module load rocm/6.1.3"
-cmd "module load cray-mpich/8.1.28"
-cmd "module load cray-python"
-#Possibly necessary exports
-#cmd "export FI_MR_CACHE_MONITOR=memhooks"
-#cmd "export FI_CXI_RX_MATCH_MODE=software"
-#cmd "export MPICH_SMP_SINGLE_COPY_MODE=NONE"
+SPACK_ENV_NAME=exawind_frontier_gpu_02_05_2025
+export EXAWIND_MANAGER=${MEMBERWORK}/cfd162/exawind-manager
+MESH_PATH=${PROJWORK}/cfd162/shared
+spec="exawind"
 
-cmd "export EXAWIND_MANAGER=${MY_EXAWIND_MANAGER}"
+cmd "module load cray-python"
 cmd "source ${EXAWIND_MANAGER}/start.sh && spack-start"
 cmd "spack env activate ${SPACK_ENV_NAME}"
-cmd "spack load ${EXAWIND_EXE}"
+cmd "spack load ${spec}"
 cmd "which exawind"
 
-cmd "srun -N 2 -n 2 --distribution=block:block --ntasks-per-node=1 --gpus-per-node=8 --gpu-bind=closest --cpu_bind=cores exawind --awind 1 --nwind 1 sphere.yaml"
+#Current possibly necessary exports
+cmd "export FI_MR_CACHE_MONITOR=memhooks"
+cmd "export FI_CXI_RX_MATCH_MODE=software"
+cmd "export MPICH_SMP_SINGLE_COPY_MODE=NONE"
+
+#Update mesh path
+sed -i "s|CHANGE_PATH|${MESH_PATH}|g" sphere-nalu.yaml || true
+
+#+amr_wind_gpu~nalu_wind_gpu
+#cmd "python3 ../tools/reorder_file.py ${SLURM_JOB_NUM_NODES}"
+#AWIND_RANKS=$((${SLURM_JOB_NUM_NODES}*8))
+#NWIND_RANKS=$((${SLURM_JOB_NUM_NODES}*56))
+#TOTAL_RANKS=$((${SLURM_JOB_NUM_NODES}*64))
+#cmd "export MPICH_RANK_REORDER_METHOD=3"
+#cmd "export MPICH_RANK_REORDER_FILE=exawind.reorder_file"
+
+#+amr_wind_gpu+nalu_wind_gpu
+cmd "export HIP_LAUNCH_BLOCKING=1"
+TOTAL_RANKS=$((${SLURM_JOB_NUM_NODES}*8))
+NWIND_RANKS=$((TOTAL_RANKS - 1))
+AWIND_RANKS=$((TOTAL_RANKS - NWIND_RANKS))
+
+cmd "srun -N${SLURM_JOB_NUM_NODES} -n${TOTAL_RANKS} --gpus-per-node=8 --gpu-bind=closest spack build-env exawind exawind --awind ${AWIND_RANKS} --nwind ${NWIND_RANKS} sphere.yaml"
